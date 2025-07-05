3254ccb504242f24f694f70bb6491c2660ab157c
Abhishekkoppaka <127208805+Abhishekkoppaka@users.noreply.github.com>
2025-07-05 12:53:53 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,3 +1,6 @@
+-----''''''''''''-
+---customer-info--
+
 ---check for nulls or duplicate
 ---- expectation: no result
 
@@ -53,3 +56,72 @@ FROM (
 	) as t
 WHERE
 	rnk =1
+
+	
+-----''''''''''''----
+
+---prd-info--
+--checking for data 
+	
+--checking for duplicates and nulls
+
+SELECT prd_id, count(*)
+FROM [silver].[crm_prd_info]
+group by prd_id
+having count(*) > 1 or prd_id IS NULL;
+
+--checking the string
+SELECT [prd_nm]
+FROM [silver].[crm_prd_info]
+WHERE TRIM(prd_nm) != prd_nm;
+
+--checking the negative numbers or null
+SELECT [prd_cost]
+FROM [silver].[crm_prd_info]
+WHERE [prd_cost]<0 OR [prd_cost] IS NULL 
+
+--stanardization and consistancy
+
+SELECT DISTINCT [prd_line]
+FROM [silver].[crm_prd_info];
+
+
+----checking invalid dates
+
+SELECT *
+FROM [silver].[crm_prd_info]
+WHERE [prd_start_dt] >
+		[prd_end_dt] 
+
+
+---Inersting prd info from bronze to silver----
+
+INSERT INTO [silver].[crm_prd_info]
+		(
+		[prd_id],
+		cat_id,
+		prd_key,
+		prd_nm,
+		[prd_cost],
+		[prd_line],
+		[prd_start_dt],
+		prd_end_dt
+		)
+
+SELECT	
+		[prd_id],
+		REPLACE(SUBSTRING([prd_key],1,5), '-','_') as cat_id,
+		SUBSTRING([prd_key],7,LEN(prd_key)) as prd_key,
+		[prd_nm],
+		ISNULL([prd_cost], 0) as [prd_cost],
+		CASE UPPER(TRIM([prd_line]))
+			WHEN 'M' THEN 'Mountain'
+			WHEN 'R' THEN 'Road'
+			WHEN 'S' THEN 'Other Sales'
+			WHEN 'T' THEN 'Touring'
+			ELSE 'na'
+		END AS [prd_line],
+		CAST([prd_start_dt] AS DATE) [prd_start_dt],
+		CAST(LEAD(prd_start_dt) OVER(partition by prd_key order by prd_start_dt asc)-1 as DATE )as prd_end_dt
+FROM [bronze].[crm_prd_info]
+
