46e16a337a22e6e9dc69432b809ad0f8c8079ead
Abhishekkoppaka <127208805+Abhishekkoppaka@users.noreply.github.com>
2025-07-05 14:31:48 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -125,3 +125,86 @@ SELECT
 		CAST(LEAD(prd_start_dt) OVER(partition by prd_key order by prd_start_dt asc)-1 as DATE )as prd_end_dt
 FROM [bronze].[crm_prd_info]
 
+---checking data
+---[crm_sales_details]
+--sales_details bronze to silver
+---checking for invlaid dates
+
+SELECT NULLIF([sls_order_dt], 0) as [sls_order_dt]
+FROM 
+[bronze].[crm_sales_details]
+WHERE 
+	[sls_order_dt] <=0 or 
+	LEN([sls_order_dt]) != 8 or
+	sls_order_dt >20500101 or
+	sls_order_dt < 20000101
+
+---checking invlaid date orders
+
+SELECT * 
+FROM [bronze].[crm_sales_details]
+WHERE [sls_order_dt] > [sls_ship_dt]
+
+--checking the business rules
+
+SELECT sls_sales AS old_sls_sales,
+		sls_quantity,
+		sls_price as old_sls_price,
+		CASE WHEN sls_sales <= 0 or sls_sales IS NULL or sls_sales != sls_quantity*abs(sls_price)  
+				THEN sls_quantity * abs(sls_price) 
+			ELSE sls_sales 
+		END as sls_sales, 
+		CASE 
+			WHEN sls_price <=0 THEN sls_price*-1
+			WHEN sls_price IS NULL or sls_price = 0 THEN sls_sales/NULLIF(sls_quantity,0)
+			ELSE sls_price
+		END as sls_price
+	
+FROM [bronze].[crm_sales_details]
+WHERE sls_sales != sls_quantity*sls_price
+	OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+	OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales,sls_quantity ,sls_price
+
+----inserting into silver sales data
+
+INSERT INTO [silver].[crm_sales_details]
+		(
+		[sls_ord_num]
+      ,[sls_prd_key]
+      ,[sls_cust_id]
+      ,[sls_order_dt]
+      ,[sls_ship_dt]
+      ,[sls_due_dt]
+      ,[sls_sales]
+      ,[sls_quantity]
+      ,[sls_price]
+
+		)
+SELECT [sls_ord_num],
+      [sls_prd_key],
+      [sls_cust_id],
+      CASE
+		WHEN  sls_order_dt = 0 or LEN(sls_order_dt) != 8 THEN  NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	  END AS [sls_order_dt],
+	  CASE
+		WHEN  [sls_ship_dt] = 0 or LEN([sls_ship_dt]) != 8 THEN  NULL
+		ELSE CAST(CAST([sls_ship_dt] AS VARCHAR) AS DATE)
+	  END AS [sls_ship_dt],
+      CASE
+		WHEN  [sls_due_dt] = 0 or LEN([sls_due_dt]) != 8 THEN  NULL
+		ELSE CAST(CAST([sls_due_dt] AS VARCHAR) AS DATE)
+	  END AS [sls_due_dt],
+      CASE WHEN sls_sales <= 0 or sls_sales IS NULL or sls_sales != sls_quantity*abs(sls_price)  
+				THEN sls_quantity * abs(sls_price) 
+			ELSE sls_sales 
+	   END as sls_sales,
+      [sls_quantity],
+      CASE 
+			WHEN sls_price <=0 THEN sls_price*-1
+			WHEN sls_price IS NULL or sls_price = 0 THEN sls_sales/NULLIF(sls_quantity,0)
+			ELSE sls_price
+		END as sls_price
+  FROM [DataWarehouse].[bronze].[crm_sales_details]
+  
