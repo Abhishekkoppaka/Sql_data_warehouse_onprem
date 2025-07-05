9785c93ba3f4f18787d9b018615514cbb91185b4
Abhishekkoppaka <127208805+Abhishekkoppaka@users.noreply.github.com>
2025-07-05 15:10:54 +0100

    Update proc_load_silver_ERP.sql

--- scripts/silver/proc_load_silver_ERP.sql
+++ scripts/silver/proc_load_silver_ERP.sql
@@ -31,4 +31,37 @@ SELECT
 	END as gen		
 FROM [bronze].[erp_cust_az12];
 
+----erp_loc
+
+---data consensitancy and standarsization
+SELECT 
+	distinct cntry as old,
+	CASE
+		WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+		WHEN TRIM(cntry) = '' or TRIM(cntry) IS NUll THEN 'na'
+		ELSE TRIM(cntry)
+	END as cntry
+
+FROM [bronze].[erp_loc_a101]
+
+
+--inserting
+INSERT INTO [silver].[erp_loc_a101]
+	(
+	cid,
+	cntry
+	)
+
+SELECT 
+	REPLACE(cid,'-','') as cid,
+	CASE
+		WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+		WHEN TRIM(cntry) = '' or TRIM(cntry) IS NUll THEN 'na'
+		ELSE TRIM(cntry)
+	END as cntry
+FROM [bronze].[erp_loc_a101]
+;
+
 
