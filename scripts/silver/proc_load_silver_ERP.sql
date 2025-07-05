53a34760c53825158bf8619b28d9d191627bba3f
Abhishekkoppaka <127208805+Abhishekkoppaka@users.noreply.github.com>
2025-07-05 14:57:48 +0100

    Update proc_load_silver_ERP.sql

--- scripts/silver/proc_load_silver_ERP.sql
+++ scripts/silver/proc_load_silver_ERP.sql
@@ -1,1 +1,34 @@
+---indentifying out of range dates
+SELECT *
+FROM [bronze].[erp_cust_az12]
+WHERE bdate >GETDATE() or bdate< '1800-01-01';
+
+
+---gender cardinality
+SELECT distinct gen
+FROM [bronze].[erp_cust_az12]
+
+--- inserting data to silver
+INSERT INTO [silver].[erp_cust_az12]
+		(
+		cid,
+		bdate,
+		gen
+		)
+SELECT 
+	CASE 
+		WHEN cid LIKE 'NAS%' THEN substring(cid,4,len(cid))
+		ELSE cid
+	END as cid,
+	CASE
+		WHEN bdate > GETDATE() THEN NULL
+		ELSE bdate
+	END as bdate,
+	CASE
+		WHEN UPPER(TRIM(gen)) in ('F','FEMALE') THEN 'Female' 
+		WHEN UPPER(TRIM(gen)) in ('M','MALE') THEN 'Male'
+		ELSE 'na'
+	END as gen		
+FROM [bronze].[erp_cust_az12];
+
 
